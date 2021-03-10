3eebeab5d54dd9174e5239d0b96f4e70c6b606b1
Fariduddin Attar Rahman <attarrahman461@gmail.com>
2021-03-11 00:47:10 +0700

    go

---
+++ I0320041_Soal2_Tugas2.py
@@ -0,0 +1,25 @@
+nama = "Fariduddin Attar Rahman"
+nim = "I0320041"
+jenis_kelamin = "Laki-laki"
+prodi = "Teknik Industri"
+angkatan = "2020"
+tempat_lahir = "Bojonegoro"
+tgl_lahir = "4 Juli 2002"
+umur = "18 tahun"
+berat_badan = "53 kg"
+tinggi_badan = "177 cm"
+ukuran_sepatu = "UK 43"
+makanan_favorit = "Mie goreng"
+alamat = "Jl.Elang C1-12 Perum UNS V Palur, Ngringo, Jaten, Karanganyar"
+hobi = "Futsal,Belajar,Membaca buku"
+lingkungan = "Rumah saya berada di perumahan tetapi dekat dengan sawah yang hijau"
+print("Nama :",nama)
+print("NIM  :",nim)
+print("Jenis kelamin :",jenis_kelamin)
+print("Alamat :",alamat)
+print("Saya adalah seorang mahasiswa",prodi,"angkatan",angkatan)
+print("Saya lahir di",tempat_lahir,"pada",tgl_lahir,"dan sekarang berumur",umur)
+print(lingkungan)
+print("Hobi saya adalah",hobi)
+print("Berat badan saya",berat_badan,"dan","tinggi saya",tinggi_badan)
+print("Ukuran sepatu saya",ukuran_sepatu)
