7b768e1c33452aff93cbc3de3b2121892a93c677
Fariduddin Attar Rahman <attarrahman461@gmail.com>
2021-03-10 18:58:29 +0700

    go

--- Soal 1 & 2.py
+++ Soal 1 & 2.py
@@ -1,5 +1,10 @@
 # Menghitung luas persegi panjang
+
 panjang=(input("Masukkan panjang :"))
 lebar=(input("Masukkan lebar :"))
 luas=int(panjang*lebar)
 print("Luas persegi panjang adalah",luas)
+
+# Menghitung luas lingkaran
+
+
